1f469b66d50e081b60735d93e725a3cf18b1673f
86fv <163122114+86fv@users.noreply.github.com>
2024-03-13 11:26:58 +0300

    Add files via upload

---
+++ timetable.php
@@ -0,0 +1,266 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<?php include("links/links.php");?>
+	<title></title>
+</head>
+<body class="container-fluid">
+<header class="bg-success text-light">DICT MODULE TWO<span class="text-end">J24</span><br></header>
+</body>
+</html>
+<?php 
+ include("links/links.php");
+$day=date("N");
+switch ($day) {
+	case '1':
+		// code...?>
+		<div class="container bg-light">
+  <h1 class="mt-4 text-center"> LEO NI MONDAY</h1>
+                        <div class="card mb-4">
+                     
+                            <div class="card-body">
+                                <table class="table table-bordered">
+                                   
+                                     <tr>
+                                       <th>C.A</th>
+                                       <td colspan="3">0730HRS-0900HRS</td>
+                                   </tr>
+                                     
+                                        <tr>
+                                       <th>SAD</th>
+                                       <td colspan="3">0900HRS-1030HRS</td>
+                                   </tr>
+                                    <tr>
+                                       <th>TUTAFIX DBMS</th>
+                                       <td colspan="3">1030HRS-1200HRS</td>
+                                   </tr>
+                                   <tr>
+                                       <th>TUNEZA FIX QT</th>
+                                       <td colspan="3">1300HRS</td>
+                                   </tr>
+                                    </tbody>
+                                </table>
+                                <a href="loguser.php" class="btn btn-danger">Exit</a>
+                            </div>
+                        </div>
+                    </div>
+<?php
+		break;
+	case '2':
+		// code...?>
+		<div class="container bg-light">
+  <h1 class="mt-4 text-center text-danger"> LEO NI TUESDAY</h1>
+                        <div class="card mb-4">
+                     
+                            <div class="card-body">
+                                <table class="table table-bordered">
+                                   
+                                     <tr>
+                                       <th>C.A</th>
+                                       <td colspan="3">0900HRS-1030HRS</td>
+                                   </tr>
+                                     
+                                        <tr>
+                                       <th>Q.T</th>
+                                       <td colspan="3">1030HRS-1200HRS</td>
+                                   </tr>
+                                    <tr>
+                                       <th>TUTAFIX SAD AMA DBMS</th>
+                                       <td colspan="3">1200HRS-1300HRS</td>
+                                   </tr>
+                                   <tr>
+                                       <th>ENDA NYUMBANI</th>
+                                       <td colspan="3">1300HRS</td>
+                                   </tr>
+                                    </tbody>
+                                </table>
+                                <a href="loguser.php" class="btn btn-danger">Exit</a>
+                            </div>
+                        </div>
+                    </div>
+<?php
+		break;
+	case '3':
+		// code...
+	?>
+		<div class="container bg-light">
+  <h1 class="mt-4 text-center"> LEO NI WEDNESDAY</h1>
+                        <div class="card mb-4">
+                     
+                            <div class="card-body">
+                                <table class="table table-bordered">
+                                   
+                                     <tr>
+                                       <th>SAD</th>
+                                       <td colspan="3">0900HRS-1030HRS</td>
+                                   </tr>
+                                     
+                                        <tr>
+                                       <th>Q.T</th>
+                                       <td colspan="3">1030HRS-1200HRS</td>
+                                   </tr>
+                                    <tr>
+                                       <th>SI TUSOME TU DBMS</th>
+                                       <td colspan="3">1200HRS-1300HRS</td>
+                                   </tr>
+                                   <tr>
+                                       <th>ENDA NYUMBANI</th>
+                                       <td colspan="3">1300HRS</td>
+                                   </tr>
+                                    </tbody>
+                                </table>
+                                <a href="loguser.php" class="btn btn-danger">Exit</a>
+                            </div>
+                        </div>
+                    </div>
+<?php
+		break;
+	case '4':
+		// code...
+	?>
+		<div class="container bg-light">
+  <h1 class="mt-4 text-center"> LEO NI THURSDAY</h1>
+                        <div class="card mb-4">
+                     
+                            <div class="card-body">
+                                <table class="table table-bordered">
+                                   
+                                     <tr>
+                                       <th>USIAMKE</th>
+                                       <td colspan="3">0900HRS-1030HRS</td>
+                                   </tr>
+                                     
+                                        <tr>
+                                       <th>AMKA UKUNYWE CHAI</th>
+                                       <td colspan="3">1030HRS-11</td>
+                                   </tr>
+                                    <tr>
+                                       <th>OOP</th>
+                                       <td colspan="3">1100HRS-1300HRS</td>
+                                   </tr>
+                                   <tr>
+                                       <th>ENDA NYUMBANI</th>
+                                       <td colspan="3">1300HRS</td>
+                                   </tr>
+                                    </tbody>
+                                </table>
+                                <a href="loguser.php" class="btn btn-danger">Exit</a>
+                            </div>
+                        </div>
+                    </div>
+<?php
+		break;
+	case '5':
+		// code...?>
+			<div class="container bg-light">
+  <h1 class="mt-4 text-center"> LEO NI FRIDAY</h1>
+                        <div class="card mb-4">
+                     
+                            <div class="card-body">
+                                <table class="table table-bordered">
+                                   
+                                     <tr>
+                                       <th>ITA MPOA</th>
+                                       <td colspan="3">0730HRS-1030HRS</td>
+                                   </tr>
+                                     
+                                        <tr>
+                                       <th>PIGA SHEREHE</th>
+                                       <td colspan="3">1030HRS-1900HRS</td>
+                                   </tr>
+                                    <tr>
+                                       <th>KULA SUPPER</th>
+                                       <td colspan="3">1900HRS-2100HRS</td>
+                                   </tr>
+                                   <tr>
+                                       <th>PIGA KITU</th>
+                                       <td colspan="3">2200HRS-0600HRS</td>
+                                   </tr>
+                                    </tbody>
+                                </table>
+                                <a href="loguser.php" class="btn btn-danger">Exit</a>
+                            </div>
+                        </div>
+                    </div>
+<?php
+
+	
+		break;
+	case '6':
+		// code...?>
+			<div class="container bg-light">
+  <h1 class="mt-4 text-center"> LEO NI SATURDAY</h1>
+                        <div class="card mb-4">
+                     
+                            <div class="card-body">
+                                <table class="table table-bordered">
+                                   
+                                     <tr>
+                                       <th>SOMA</th>
+                                       <td colspan="3">0730HRS-1030HRS</td>
+                                   </tr>
+                                     
+                                        <tr>
+                                       <th>SOMA KABISA</th>
+                                       <td colspan="3">1030HRS-1900HRS</td>
+                                   </tr>
+                                    <tr>
+                                       <th>SOMA BANA</th>
+                                       <td colspan="3">1900HRS-2100HRS</td>
+                                   </tr>
+                                   <tr>
+                                       <th>PIGA KITU</th>
+                                       <td colspan="3">2200HRS-0600HRS</td>
+                                   </tr>
+                                    </tbody>
+                                </table>
+                                <a href="loguser.php" class="btn btn-danger">Exit</a>
+                            </div>
+                        </div>
+                    </div>
+<?php
+		break;
+	case '7':
+		// code...
+	?>
+			<div class="container bg-light">
+  <h1 class="mt-4 text-center"> LEO NI SUNDAY</h1>
+                        <div class="card mb-4">
+                     
+                            <div class="card-body">
+                                <table class="table table-bordered">
+                                   
+                                     <tr>
+                                       <th>ENDA CHURCH</th>
+                                       <td colspan="3">0730HRS-1030HRS</td>
+                                   </tr>
+                                     
+                                        <tr>
+                                       <th>INAFA UKUE CHURCH</th>
+                                       <td colspan="3">1030HRS-1900HRS</td>
+                                   </tr>
+                                    <tr>
+                                       <th>KULA SUPPER</th>
+                                       <td colspan="3">1900HRS-2100HRS</td>
+                                   </tr>
+                                   <tr>
+                                       <th>SOMA</th>
+                                       <td colspan="3">2200HRS-0600HRS</td>
+                                   </tr>
+                                    </tbody>
+                                </table>
+                                <a href="loguser.php" class="btn btn-danger">Exit</a>
+                            </div>
+                        </div>
+                    </div>
+<?php
+		break;
+	
+	default:
+		// code...
+	echo "KING IAN";
+		break;
+}
+ ?>
